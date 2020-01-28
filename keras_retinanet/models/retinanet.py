17468d85d429c68511fa4b7f33bfd2951ae3c6a8
armanmohammadi704 <59266965+armanmohammadi704@users.noreply.github.com>
2020-01-28 23:24:46 +0330

    Update retinanet.py

--- keras_retinanet/models/retinanet.py
+++ keras_retinanet/models/retinanet.py
@@ -42,18 +42,18 @@ def default_classification_model(
         A keras.models.Model that predicts classes for each anchor.
     """
     options = {
-        'kernel_size' : 3,
+        'kernel_size' : (3,3,1),
         'strides'     : 1,
         'padding'     : 'same',
     }
 
     if keras.backend.image_data_format() == 'channels_first':
-        inputs  = keras.layers.Input(shape=(pyramid_feature_size, None, None))
+        inputs  = keras.layers.Input(shape=(pyramid_feature_size, None, None,None))
     else:
-        inputs  = keras.layers.Input(shape=(None, None, pyramid_feature_size))
+        inputs  = keras.layers.Input(shape=(None,None, None, pyramid_feature_size))
     outputs = inputs
     for i in range(4):
-        outputs = keras.layers.Conv2D(
+        outputs = keras.layers.Conv3D(
             filters=classification_feature_size,
             activation='relu',
             name='pyramid_classification_{}'.format(i),
@@ -62,7 +62,7 @@ def default_classification_model(
             **options
         )(outputs)
 
-    outputs = keras.layers.Conv2D(
+    outputs = keras.layers.Conv3D(
         filters=num_classes * num_anchors,
         kernel_initializer=keras.initializers.normal(mean=0.0, stddev=0.01, seed=None),
         bias_initializer=initializers.PriorProbability(probability=prior_probability),
@@ -96,7 +96,7 @@ def default_regression_model(num_values, num_anchors, pyramid_feature_size=256, 
     # RetinaNet (classification) subnets are initialized
     # with bias b = 0 and a Gaussian weight fill with stddev = 0.01.
     options = {
-        'kernel_size'        : 3,
+        'kernel_size'        : (3,3,1),
         'strides'            : 1,
         'padding'            : 'same',
         'kernel_initializer' : keras.initializers.normal(mean=0.0, stddev=0.01, seed=None),
@@ -104,19 +104,19 @@ def default_regression_model(num_values, num_anchors, pyramid_feature_size=256, 
     }
 
     if keras.backend.image_data_format() == 'channels_first':
-        inputs  = keras.layers.Input(shape=(pyramid_feature_size, None, None))
+        inputs  = keras.layers.Input(shape=(pyramid_feature_size, None, None,None))
     else:
-        inputs  = keras.layers.Input(shape=(None, None, pyramid_feature_size))
+        inputs  = keras.layers.Input(shape=(None,None, None, pyramid_feature_size))
     outputs = inputs
     for i in range(4):
-        outputs = keras.layers.Conv2D(
+        outputs = keras.layers.Conv3D(
             filters=regression_feature_size,
             activation='relu',
             name='pyramid_regression_{}'.format(i),
             **options
         )(outputs)
 
-    outputs = keras.layers.Conv2D(num_anchors * num_values, name='pyramid_regression', **options)(outputs)
+    outputs = keras.layers.Conv3D(num_anchors * num_values, name='pyramid_regression', **options)(outputs)
     if keras.backend.image_data_format() == 'channels_first':
         outputs = keras.layers.Permute((2, 3, 1), name='pyramid_regression_permute')(outputs)
     outputs = keras.layers.Reshape((-1, num_values), name='pyramid_regression_reshape')(outputs)
