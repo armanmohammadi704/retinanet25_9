891aa56ce338a8c40dee71953a4b9a1653b20e6e
abattar <60582502+abattar@users.noreply.github.com>
2020-02-05 22:22:32 +0330

    Update retinanet.py

--- keras_retinanet/models/retinanet.py
+++ keras_retinanet/models/retinanet.py
@@ -63,7 +63,7 @@ def default_classification_model(
         )(outputs)
 
     outputs = keras.layers.Conv3D(
-        filters=num_classes * num_anchors,
+        filters=1,#num_classes * num_anchors,
         kernel_initializer=keras.initializers.normal(mean=0.0, stddev=0.01, seed=None),
         bias_initializer=initializers.PriorProbability(probability=prior_probability),
         name='pyramid_classification',
@@ -116,7 +116,7 @@ def default_regression_model(num_values, num_anchors, pyramid_feature_size=256, 
             **options
         )(outputs)
 
-    outputs = keras.layers.Conv3D(num_anchors * num_values, name='pyramid_regression', **options)(outputs)
+    outputs = keras.layers.Conv3D(4, name='pyramid_regression', **options)(outputs)
     if keras.backend.image_data_format() == 'channels_first':
         outputs = keras.layers.Permute((2, 3, 1), name='pyramid_regression_permute')(outputs)
     outputs = keras.layers.Reshape((-1, num_values), name='pyramid_regression_reshape')(outputs)
