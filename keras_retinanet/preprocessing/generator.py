4c30f31e5a96b7c497673478b4ba82647bbef8f8
abattar <60582502+abattar@users.noreply.github.com>
2020-02-06 22:15:51 +0330

    Update generator.py

--- keras_retinanet/preprocessing/generator.py
+++ keras_retinanet/preprocessing/generator.py
@@ -267,7 +267,7 @@ class Generator(keras.utils.Sequence):
 
         # copy all images to the upper left part of the image batch object
         for image_index, image in enumerate(image_group):
-            image_batch[image_index, :image.shape[0], :image.shape[1], :image.shape[2]] = image
+            image_batch[image_index, :image.shape[0], :image.shape[1], :image.shape[2]] = image[:,:,:,0]
 
         if keras.backend.image_data_format() == 'channels_first':
             image_batch = image_batch.transpose((0, 3, 1, 2))
